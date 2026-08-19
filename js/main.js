061881bb9c2bef02d9cc75abe7ea8e0323edd69d
7evenCity <253611841+7evenCity@users.noreply.github.com>
2026-08-19 02:16:21 +0100

    Point Discord webhook to Buyers channel

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
     ekitemacro:   { name: 'elitemacro',                price: 9.99 },
     elitetweak:   { name: 'EliteTweak (FPS Booster)',  price: 9.99 }
   };
-  var DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1539437794999402578/Sjkz0mWV7CIqK4YHlWMAcfj7B9poP_BGegVUuj9n49NHPhPOFL0QXrGZAKbl9KdeHtp4';
+  var DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1539441523861487707/_h8lqrk4a460SRBTWZejwTtRyUfHbnaiJ4pTSTooOhp1SkCcIlh1w--SvdYzdaQ-6dPT';
   var DISCORD_INVITE_URL = 'https://discord.gg/6gjzHZteXV';
 
   var navToggle = document.getElementById('navToggle');
